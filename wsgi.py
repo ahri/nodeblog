c25fccd24d1ea94fd8bdbda426dbfe87f4da9844
Adam Piper <adam@ahri.net>
2011-10-14 16:44:36 +0100

    Remove dotcloud stuff

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from blog import app as application
-
 if __name__ == "__main__":
     application.debug = True
     application.run(host='0.0.0.0', port=8000)
